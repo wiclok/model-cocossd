9d0279face2a34a46a50a0d0333c2e40de563554
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-06-18 15:10:07 -0300

    proyect finalized

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { ObjectDetection } from "./ObjectDetection"
+import './assets/styles/app.css'
+
+function App() {
+  
+  return (
+    <div className="app">
+      <h1 className="Title">Image Object Detection</h1>
+      <ObjectDetection />
+    </div>
+  )
+}
+
+export default App
